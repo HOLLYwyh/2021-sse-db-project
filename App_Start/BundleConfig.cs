2c4ed10cdd98505ed0389c96214eebc93852374f
HOLLYwyh <1689394668@qq.com>
2021-04-29 22:31:43 +0800

    ProjectTemplate

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -23,8 +23,7 @@ namespace InternetMall
                 "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                "~/Content/bootstrap.css",
-                "~/Content/site.css"));
+                "~/Content/bootstrap.css"));
         }
     }
 }
