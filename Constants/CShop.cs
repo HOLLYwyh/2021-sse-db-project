77af23fd63985dbcd4f81e2e4fb1e42d56474071
HOLLYwyh <1689394668@qq.com>
2021-05-30 13:12:53 +0800

    ADD Constants

---
+++ Constants/CShop.cs
@@ -0,0 +1,12 @@
+//商店相关常量
+namespace InternetMall.Constants.Shop
+{
+    public class CShop
+    {
+        public const int Unknown = 0; //未定义
+        public const int OfficialFlagship = 1; //未定义
+        public const int PlatformAuth = 2; //未定义
+        public const int Individual = 3; //未定义
+        public const int BannedShop = 4; //未定义
+    }
+}
