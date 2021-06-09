f6bdd2ff17139ceb329163e8bdf675cae7aab538
Sherlock White <1172940427@qq.com>
2021-06-09 18:09:53 +0800

    update

---
+++ Scripts/SellerBackground/sidebar.js
@@ -0,0 +1,8 @@
+Vue.component('sidebar', {
+    data: function () {
+      return {
+        count: 0
+      }
+    },
+    template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+})
