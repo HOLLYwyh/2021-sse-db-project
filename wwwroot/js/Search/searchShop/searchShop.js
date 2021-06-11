3115036ff94f343c7c878d2210bf587d990f361d
HOLLYwyh <1689394668@qq.com>
2021-06-11 21:57:18 +0800

    update searchShop page

--- wwwroot/js/Search/searchShop/searchShop.js
+++ wwwroot/js/Search/searchShop/searchShop.js
@@ -2,3 +2,21 @@
 new Vue({
     el: "#shortcutlg"
 })
+
+//搜索框
+new Vue({
+    el:"#search",
+    data : {
+        input:''
+    }
+})
+
+//搜索按钮
+new Vue({
+    el:"#search-btn"
+})
+
+//购物车按钮
+new Vue({
+    el:"#shopping-cart"
+})
