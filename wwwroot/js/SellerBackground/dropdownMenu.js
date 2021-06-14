8d8d7cbec0198c032aa93836c8e3d84cdb742e3b
Sherlock White <1172940427@qq.com>
2021-06-14 17:26:53 +0800

    下拉菜单基本ok
    封进了component

--- wwwroot/js/SellerBackground/dropdownMenu.js
+++ wwwroot/js/SellerBackground/dropdownMenu.js
@@ -1,14 +1,14 @@
 Vue.component('dropdown-menu', {
     template: `
-        <div>
-            <button class="dropbtn"></button>
-            <div class="dropdown-content">
-                <a href="#">个人信息</a>
-                <a href="#">设置</a>
-                <a href="#">退出登录</a>
-            </div>
+    <div class="dropdown">
+        <button class="dropbtn"></button>
+        <div class="dropdown-content">
+            <a href="#">个人信息</a>
+            <a href="#">设置</a>
+            <a href="#">退出登录</a>
         </div>
+    </div>
     `
 })
 
-//var dropdown = new Vue({ el: '#dropdown' });
+let menu = new Vue({ el: '#menu' });
