bff6a45d2da897a87fd66121fbf0f33ccbdc96cd
HOLLYwyh <1689394668@qq.com>
2021-06-14 23:31:19 +0800

    fix some displays of backToTop rockets

--- wwwroot/js/Search/searchCommodity/searchCommodity.js
+++ wwwroot/js/Search/searchCommodity/searchCommodity.js
@@ -7,3 +7,28 @@ new Vue({
 new Vue({
     el:"#search-bar"
 })
+
+//翻页
+new Vue({
+    el: "#turn-to-page",
+        methods: {
+            handleSizeChange(val) {
+                console.log(`每页 ${val} 条`);
+            },
+            handleCurrentChange(val) {
+                console.log(`当前页: ${val}`);
+            }
+        },
+        data:{
+            currentPage1: 1,
+        }
+})
+
+
+//测试卡片
+new Vue({
+    el: "#test",
+    data: {
+           currentDate: new Date()
+    }
+})
