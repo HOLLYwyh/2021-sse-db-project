2061970857cf86566d39686dfd419054bcdddaf6
DukeofCambridge <68137344+DukeofCambridge@users.noreply.github.com>
2021-06-23 19:44:02 +0800

    图片删除

--- wwwroot/js/Favorites/follow/follow.js
+++ wwwroot/js/Favorites/follow/follow.js
@@ -102,16 +102,6 @@ new Vue({
             rate: 4.9,
         };
     },
-    methods: {
-        startHacking() {
-            this.$notify({
-                title: "It works!",
-                type: "success",
-                message:
-                    "We've laid the ground work for you. It's time for you to build something epic!",
-                duration: 5000,
-            });
-        },
-    }
+    methods: {}
 
 })
