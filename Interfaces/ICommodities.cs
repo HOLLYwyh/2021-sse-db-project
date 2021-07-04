c5394db49e263cd913f5f2fd617b90abf60e474e
HJK <75560056+1654364091@users.noreply.github.com>
2021-07-04 23:14:46 +0800

    Add files via upload

---
+++ Interfaces/ICommodities.cs
@@ -0,0 +1,15 @@
+﻿using InternetMall.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InternetMall.Interfaces
+{
+    interface ICommodities
+    {
+        public  Task<bool> Create(decimal price, string category, string description, int storage, string name, string shop_id,string url);
+        public  Task<bool> Delete(string shop_id, string commodity_id);
+        public Task<List<Commodity>> ShowCommodities(string shopId, string searchCondition);
+    }
+}
