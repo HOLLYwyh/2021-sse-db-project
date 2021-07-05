870290d1387b6424e092b3d7cd3df74f35b5c514
lif314 <2216155077@qq.com>
2021-07-05 16:48:20 +0800

    Update ICartService.cs

--- Interfaces/ICartService.cs
+++ Interfaces/ICartService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using InternetMall.Models.BusinessEntity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,6 +11,17 @@ namespace InternetMall.Interfaces
     /// </summary>
     public interface ICartService
     {
+        //添加购物车
+        public void addToCart(string buyerid, string commodityid);
 
+        // 从购物车中删除
+        public Task RemoveFromCart(string buyerid, string commodityid);
+       
+        // 清除购物车
+        public Task RemoveAllCart(string buyerid);
+        
+        // 查看购物车
+        public IEnumerable<CartView> GetCartProduct(string buyerid);
+       
     }
 }
