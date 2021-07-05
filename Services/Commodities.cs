ed5323348aabef16eaec92a1cda69e0ac8b3bfad
1654364091 <1654364091@qq.com>
2021-07-05 20:01:34 +0800

    补上漏掉的一两个using引用

--- Services/Commodities.cs
+++ Services/Commodities.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using InternetMall.DBContext;
 using InternetMall.Interfaces;
 using InternetMall.Models;
 using Microsoft.EntityFrameworkCore;
