7fad1899deaa3fb4f812243ff4c3249710d27a19
lif314 <2216155077@qq.com>
2021-07-05 19:13:12 +0800

    update SecurityService
    更新安全管理

--- Interfaces/ISecurityService.cs
+++ Interfaces/ISecurityService.cs
@@ -7,16 +7,16 @@ namespace InternetMall.Interfaces
 {
     public interface ISecurityService
     {
-        // 显示用户电话号码：不存在返回"null"
-        public string displayPhone(string id);
+        // 显示用户电话号码：不存在返回null
+        public string displayPhone(string buyerid);
 
         // 显示用户密码
-        public string displayPasswd(string id);
+        public string displayPasswd(string buyerid);
 
         // 修改用户绑定的手机号码
-        public Task<int> updatePhone(string id, string newPhone);
+        public Task<bool> updatePhone(string buyerid, string oldPhone, string newPhone);
 
         // 修改用户密码
-        public Task<int> updatePasswd(string id, string newPasswd);
+        public Task<bool> updatePasswd(string buyerid, string oldPasswd, string newPasswd);
     }
 }
--- Services/SecurityService.cs
+++ Services/SecurityService.cs
@@ -20,41 +20,70 @@ namespace InternetMall.Services
         }
 
         // 显示用户绑定手机号
-        public string displayPhone(string id)
+        public string displayPhone(string buyerid)
         {
             // 检测用户是否存在，但应该没有必要，因为已经登录
             /*if(_ctx.Buyers.Any(e => e.BuyerId == id))
             */
-            var buyer = _ctx.Buyers.FirstOrDefault(x => x.BuyerId == id);
+            Buyer buyer = _ctx.Buyers.FirstOrDefault(x => x.BuyerId == buyerid);
             // 电话号码需要检测是否存在
             if (buyer.Phone != null)
                 return buyer.Phone;
             else
-                return "null";
+                return null;
         }
 
         // 显示用户密码
-        public  string displayPasswd(string id)
+        public string displayPasswd(string buyerid)
         {
-            var buyer = _ctx.Buyers.FirstOrDefault(x => x.BuyerId == id);
+            Buyer buyer = _ctx.Buyers.FirstOrDefault(x => x.BuyerId == buyerid);
             return buyer.Passwd;
         }
 
         // 修改用户绑定的手机号码
-        public Task<int> updatePhone(string id, string newPhone)
+        public async Task<bool> updatePhone(string buyerid, string oldPhone, string newPhone)
         {
+            Buyer buyer = _ctx.Buyers.FirstOrDefault(x => x.BuyerId == buyerid);   
 
-            var buyer = _ctx.Buyers.FirstOrDefault(x => x.BuyerId == id);
-            buyer.Passwd = newPhone;
-           return _ctx.SaveChangesAsync();
+            if (buyer == null)
+            {
+                return false;
+                throw new DllNotFoundException();
+            }
+            else
+            {
+                if (buyer.Phone == null || buyer.Phone == oldPhone)  // 比对电话号码
+                {
+                    buyer.Phone = newPhone;
+                    await _ctx.SaveChangesAsync();
+                    return true;
+                }
+                else
+                    return false;
+            }
         }
 
         // 修改用户密码
-        public Task<int> updatePasswd(string id, string newPasswd)
+        public async Task<bool> updatePasswd(string buyerid, string oldPasswd, string newPasswd)
         {
-            var buyer = _ctx.Buyers.FirstOrDefault(x => x.BuyerId == id);
-            buyer.Passwd = newPasswd;
-            return _ctx.SaveChangesAsync();
+            Buyer buyer = _ctx.Buyers.FirstOrDefault(x => x.BuyerId == buyerid);
+
+            if (buyer == null)
+            {
+                return false;
+                throw new DllNotFoundException();
+            }
+            else
+            {
+                if (buyer.Passwd == oldPasswd)  // 比对密码
+                {
+                    buyer.Passwd = newPasswd;
+                    await _ctx.SaveChangesAsync();
+                    return true;
+                }
+                else
+                    return false;
+            }
         }
     }
 }
