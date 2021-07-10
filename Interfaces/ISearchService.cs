72313f5cdf6ea4c8385dd238b45aa52736b82cbd
卢子昂 <75532785+Strange-tech@users.noreply.github.com>
2021-07-10 20:55:15 +0800

    Update ISearchService.cs

--- Interfaces/ISearchService.cs
+++ Interfaces/ISearchService.cs
@@ -1,17 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using InternetMall.Models;
-using Microsoft.AspNetCore.Mvc;
 
-namespace Internetmall.Interfaces
-{
-    interface ISearchService
-    {
-        public List<Shop> SearchShop(string shopName);
-
-        public List<Commodity> SearchCommodity(string commodityName);
-
-    }
-}
