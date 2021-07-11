644620046d321d3151e2c627324ebc25fe4538ab
lif314 <2216155077@qq.com>
2021-07-11 22:08:36 +0800

    OrdeView
    更新Order类型数据

--- Models/OrdersCommodity.cs
+++ Models/OrdersCommodity.cs
@@ -9,7 +9,7 @@ namespace InternetMall.Models
     {
         public string OrdersId { get; set; }
         public string CommodityId { get; set; }
-        public int? Status { get; set; }
+        public int? Status { get; set; }           // 订单里物品的状态
 
         public virtual Commodity Commodity { get; set; }
         public virtual Order Orders { get; set; }
