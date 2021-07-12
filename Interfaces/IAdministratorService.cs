e8a863735b088174b8f625ea95e5c1d097de84c8
Strange <939543598@qq.com>
2021-07-12 14:50:08 +0800

    管理员

---
+++ Interfaces/IAdministratorService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InternetMall.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Internetmall.Interfaces
+{
+    interface IAdministratorService
+    {
+        public bool SignUp(string phone, string nickName, string passwd);//注册
+
+        public Administrator Login(string s, string passwd);//登录
+    }
+}
