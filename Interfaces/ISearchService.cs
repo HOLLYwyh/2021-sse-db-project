d186df976c205e4c9fcdbd391e29bd135b91d1a8
卢子昂 <75532785+Strange-tech@users.noreply.github.com>
2021-07-13 10:07:53 +0800

    Update ISearchService.cs

--- Interfaces/ISearchService.cs
+++ Interfaces/ISearchService.cs
@@ -1,18 +1,1 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Internetmall.Models.BusinessEntity;
-using InternetMall.Models;
-using Microsoft.AspNetCore.Mvc;
 
-namespace Internetmall.Interfaces
-{
-    interface ISearchService
-    {
-        //注释
-        public List<Good> SearchCommodity(string commodityName, int searchType = 0);
-        //注释
-        public List<ShopView> SearchShop(string shopName, int searchType = 0);
-    }
-}
